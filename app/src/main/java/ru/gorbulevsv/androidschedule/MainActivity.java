939948c8112899bc1a714ed9c4d9b9df1c85cbb2
Горбулёв Сергей <gorbulevsv@yandex.ru>
2022-11-23 16:48:35 +0300

    Initial commit

---
+++ app/src/main/java/ru/gorbulevsv/androidschedule/MainActivity.java
@@ -0,0 +1,78 @@
+package ru.gorbulevsv.androidschedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.DownloadListener;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.AbsListView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    SwipeRefreshLayout swipeRefreshLayout;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://svelte-hram.netlify.app/android/schedule");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                if (!isNetworkAvailable()) {
+                    webView.loadUrl("file:///android_asset/error.html");
+                }
+                else{
+                    webView.loadUrl("https://svelte-hram.netlify.app/android/schedule");
+                }
+            }
+        });
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            if (!isNetworkAvailable()){
+                webView.loadUrl("file:///android_asset/error.html");
+            }
+            else{
+                webView.loadUrl("https://svelte-hram.netlify.app/android/schedule");
+            }
+        });
+
+
+    }
+
+    public boolean isNetworkAvailable() {
+        // Get Connectivity Manager class object from Systems Service
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get Network Info from connectivity Manager
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        // if no network is available networkInfo will be null
+        // otherwise check if we are connected
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+}
